1c5857e5dbe3fd84fc92d1af6bcc718fb485d67e
Corinna Vinschen <xda@vinschen.de>
2017-08-12 21:22:57 +0200

    InputDispatcher: On keypress, deliver keycode to pokeUserActivity
    Change-Id: I8f64a6fa9b4b2e4520e25731f55e89f5087c70da
    Signed-off-by: Corinna Vinschen <xda@vinschen.de>

--- services/inputflinger/dispatcher/InputDispatcher.cpp
+++ services/inputflinger/dispatcher/InputDispatcher.cpp
@@ -3169,6 +3169,7 @@ void InputDispatcher::pokeUserActivityLocked(const EventEntry& eventEntry) {
     }
 
     int32_t eventType = USER_ACTIVITY_EVENT_OTHER;
+    int32_t keyCode = AKEYCODE_UNKNOWN;
     switch (eventEntry.type) {
         case EventEntry::Type::MOTION: {
             const MotionEntry& motionEntry = static_cast<const MotionEntry&>(eventEntry);
@@ -3209,6 +3210,7 @@ void InputDispatcher::pokeUserActivityLocked(const EventEntry& eventEntry) {
             }
 
             eventType = USER_ACTIVITY_EVENT_BUTTON;
+            keyCode = keyEntry.keyCode;
             break;
         }
         default: {
@@ -3218,10 +3220,10 @@ void InputDispatcher::pokeUserActivityLocked(const EventEntry& eventEntry) {
         }
     }
 
-    auto command = [this, eventTime = eventEntry.eventTime, eventType, displayId]()
+    auto command = [this, eventTime = eventEntry.eventTime, eventType, displayId, keyCode]()
                            REQUIRES(mLock) {
                                scoped_unlock unlock(mLock);
-                               mPolicy.pokeUserActivity(eventTime, eventType, displayId);
+                               mPolicy.pokeUserActivity(eventTime, eventType, displayId, keyCode);
                            };
     postCommandLocked(std::move(command));
 }
